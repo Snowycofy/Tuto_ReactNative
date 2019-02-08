fe3ca4e37e094612fb526582d1cb35d7ebe3d5cd
Jonathan Boerlen <jonathan.boerlen@hstucia.fr>
2019-02-08 11:53:36 +0100

    Testing animations / final Part (PanResponder)

--- Components/Test.js
+++ Components/Test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, Platform, Text, Animated, Easing } from 'react-native'
+import { StyleSheet, View, Platform, Text, Animated, Easing, PanResponder, Dimensions } from 'react-native'
 import HelloWorld from './HelloWorld';
 import { relativeTimeThreshold } from 'moment';
 
@@ -8,87 +8,35 @@ class Test extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            topPosition : new Animated.Value(0),
-            leftPosition : new Animated.Value(0)
+            topPosition : 0,
+            leftPosition : 0
         }
-    }
 
-    componentDidMount() {
+        var {height, width} = Dimensions.get('window');
 
-        {/*Animated.timing(
-            this.state.topPosition,
-            {
-                toValue: 100,
-                duration: 3000,
-                easing: Easing.bounce,
+        this.panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: (evt, gestureState) => true,
+            onPanResponderMove: (evt, gestureState) => {
+                let touches = evt.nativeEvent.touches;
+                if (touches.length == 1)
+                {
+                    this.setState({
+                        topPosition: touches[0].pageY - height/2,
+                        leftPosition: touches[0].pageX - width/2
+                    })
+                }
+
             }
-        ).start()
-        */}
-        {/*Animated.spring(
-            this.state.topPosition,
-            {
-                toValue: 100,
-                speed: 4,
-                bounciness: 30,
-            }
-        ).start()
-        */}
-        {/*Animated.decay(
-            this.state.topPosition,
-            {
-                velocity: 0.8,
-                deceleration: 0.997
-            }
-        ).start()
-        */}
-        {/*Animated.sequence([
-            Animated.spring(
-                this.state.topPosition,
-                {
-                    toValue: 100,
-                    tension: 8,
-                    friction: 3
-                }
-            ),
-            Animated.timing(
-                this.state.topPosition,
-                {
-                    toValue: 0,
-                    duration: 1000,
-                    easing: Easing.elastic(2),
-                }
-            )]).start()
-            */}
-        Animated.parallel([
-            Animated.spring(
-                this.state.topPosition,
-                {
-                    toValue: 100,
-                    tension: 8,
-                    friction: 3
-                }
-            ),
-            Animated.timing(
-                this.state.leftPosition,
-                {
-                    toValue: 100,
-                    duration: 1000,
-                    easing: Easing.elastic(2),
-                }
-            )
-        ]).start()
+        })
     }
 
     render() {
     return (
       <View style={styles.main_container}>
-        <Animated.View style={[styles.animation_view, {top: this.state.topPosition, left: this.state.leftPosition}]}>
-            {/*
-                Platform.OS === 'ios' ? <Text>iOS</Text> : <Text>Android</Text>
-            
-            <HelloWorld/>
-            */}
-        </Animated.View>
+        <View
+            {...this.panResponder.panHandlers}
+            style= {[styles.animation_view , {top : this.state.topPosition, left: this.state.leftPosition}]}>
+        </View>
       </View>
     )
   }
@@ -104,31 +52,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
     width: 100,
     height: 100
-  },
-  subview_container: {
-    ...Platform.select({
-        ios: {
-            backgroundColor: 'red',
-            height: 100,
-            width: 50
-        },
-        android: {
-            backgroundColor: 'red',
-            height: 50,
-            width: 100
-        }
-    })
-    }})
-
-{/* 
-        ou 
-        subview_container: {
-            backgroundColor: Platform.OS === 'ios' ? 'red' : 'blue,
-            height: Platform.OS === 'ios' ?  100 : 50,
-            width: Platform.OS === 'ios' ?  50 : 100
-        }
-
-*/}
+  }
+})
 
 
 export default Test
