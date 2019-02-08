7c239fa16ab439a5edee10f50208c7d6095475dd
Jonathan Boerlen <jonathan.boerlen@hstucia.fr>
2019-02-08 11:32:33 +0100

    Testing animations / First Part (Animated.sequence)

--- Components/Test.js
+++ Components/Test.js
@@ -1,18 +1,73 @@
 import React from 'react'
-import { StyleSheet, View, Platform, Text } from 'react-native'
+import { StyleSheet, View, Platform, Text, Animated, Easing } from 'react-native'
 import HelloWorld from './HelloWorld';
 
 class Test extends React.Component {
 
-  render() {
+    constructor(props){
+        super(props)
+        this.state = {
+            topPosition : new Animated.Value(0)
+        }
+    }
+
+    componentDidMount() {
+
+        {/*Animated.timing(
+            this.state.topPosition,
+            {
+                toValue: 100,
+                duration: 3000,
+                easing: Easing.bounce,
+            }
+        ).start()
+        */}
+        {/*Animated.spring(
+            this.state.topPosition,
+            {
+                toValue: 100,
+                speed: 4,
+                bounciness: 30,
+            }
+        ).start()
+        */}
+        {/*Animated.decay(
+            this.state.topPosition,
+            {
+                velocity: 0.8,
+                deceleration: 0.997
+            }
+        ).start()
+        */}
+        Animated.sequence([
+            Animated.spring(
+                this.state.topPosition,
+                {
+                    toValue: 100,
+                    tension: 8,
+                    friction: 3
+                }
+            ),
+            Animated.timing(
+                this.state.topPosition,
+                {
+                    toValue: 0,
+                    duration: 1000,
+                    easing: Easing.elastic(2),
+                }
+            )]).start()
+    }
+
+    render() {
     return (
       <View style={styles.main_container}>
-        <View style={styles.subview_container}>
+        <Animated.View style={[styles.animation_view, {top: this.state.topPosition}]}>
             {/*
                 Platform.OS === 'ios' ? <Text>iOS</Text> : <Text>Android</Text>
-            */}
+            
             <HelloWorld/>
-        </View>
+            */}
+        </Animated.View>
       </View>
     )
   }
@@ -23,6 +78,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  animation_view: {
+    backgroundColor: 'red',
+    width: 100,
+    height: 100
   },
   subview_container: {
     ...Platform.select({
